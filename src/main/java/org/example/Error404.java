cc68e10b02d1e6bc0c1686d36a716fbd4ee422a6
Hari-deve <hari.padmanaban@anywhere.co>
2023-06-01 11:25:20 +0530

    Error code updated

--- src/main/java/org/example/Error404.java
+++ src/main/java/org/example/Error404.java
@@ -1,5 +1,4 @@
 package org.example;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +11,16 @@ public class Error404 extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out=resp.getWriter();
-        out.println("No such file found");
+        resp.setContentType("text/html");
+        Integer status = (Integer) req.getAttribute("javax.servlet.error.status_code");
+
+        String reqURI = (String) req.getAttribute("javax.servlet.error.request_uri");
+        out.println("Error code: "+status+"<br></br>");
+        out.println("Error URI: "+reqURI+"<br></br>");
+        out.println("No such file found"+"<br></br>");
+        out.println("<form action = 'home'>");
+        out.println("<input type='submit' value='home'>");
+        out.println("</form>");
+
     }
 }
--- src/main/java/org/example/ErrorPage.java
+++ src/main/java/org/example/ErrorPage.java
@@ -13,8 +13,13 @@ public class ErrorPage extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
+        Integer status = (Integer) req.getAttribute("javax.servlet.error.status_code");
+
+        Throwable exceptionName = (Throwable) req.getAttribute("javax.servlet.error.exception");
         PrintWriter out=resp.getWriter();
-        out.println("You have entered wrong Inputs Check Again");
+        out.println("Error code: "+status+"<br></br>");
+        out.println("Exception Occurred: "+exceptionName+"<br></br>");
+        out.println("You have entered wrong Inputs Check Again"+"<br></br>");
         out.println("<form action = 'home'>");
         out.println("<input type='submit' value='home'>");
         out.println("</form>");
